6e604badc7b1956f2b13859b715c73f06ad772c7
Honda-Takumi <39166057+Honda-Takumi@users.noreply.github.com>
2025-10-28 02:31:31 +0900

    サンプルのsln追加 (#1)

---
+++ JSTModule.Api/JSTModule.Api/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JSTModule.Api;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var now = JSTModule.domain.JST.Now();
+        Console.WriteLine(now.ToJapaneseString());
+    }
+}
